c0bebfd531e25dd820ec3202d8adae8e804f8414
Karl.Kain <57959111+KarlKain@users.noreply.github.com>
2021-12-16 10:01:56 -0500

    Update Day1Part1.py

--- Day1Part1.py
+++ Day1Part1.py
@@ -1,2 +1,16 @@
-AOC.py
+file = open('Day1Part1.txt','r')
+depths = file.readlines()
+
+total_increasing = 0
+
+for i in range(len(depths)):
+    if i == 0:
+        continue 
+    
+    if depths[i] > depths[i - 1]:
+      total_increasing += 1
+      
+print (total_increasing)
+      
+      
 
